486ee305b0faf44f3f841163b6f038fa683978dd
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2021-07-09 15:23:14 +0530

    Add files via upload

---
+++ Avatar Creator/script.js
@@ -0,0 +1,246 @@
+function face () {
+	document.getElementById('fface').style.bottom = '2%';
+	document.getElementById('fhair').style.bottom = '-32%';
+	document.getElementById('feyes').style.bottom = '-32%';
+	document.getElementById('febrow').style.bottom = '-32%';
+	document.getElementById('flips').style.bottom = '-32%';
+	document.getElementById('fbeard').style.bottom = '-32%'; 
+}
+function hair () {
+	document.getElementById('fface').style.bottom = '-32%';
+	document.getElementById('fhair').style.bottom = '2%';
+	document.getElementById('feyes').style.bottom = '-32%';
+	document.getElementById('febrow').style.bottom = '-32%';
+	document.getElementById('flips').style.bottom = '-32%';
+	document.getElementById('fbeard').style.bottom = '-32%'; 
+}
+function eyes () {
+	document.getElementById('fface').style.bottom = '-32%';
+	document.getElementById('fhair').style.bottom = '-32%';
+	document.getElementById('feyes').style.bottom = '2%';
+	document.getElementById('febrow').style.bottom = '-32%';
+	document.getElementById('flips').style.bottom = '-32%';
+	document.getElementById('fbeard').style.bottom = '-32%'; 
+}
+function ebrow () {
+	document.getElementById('fface').style.bottom = '-32%';
+	document.getElementById('fhair').style.bottom = '-32%';
+	document.getElementById('feyes').style.bottom = '-32%';
+	document.getElementById('febrow').style.bottom = '2%';
+	document.getElementById('flips').style.bottom = '-32%';
+	document.getElementById('fbeard').style.bottom = '-32%'; 
+}
+function lips () {
+	document.getElementById('fface').style.bottom = '-32%';
+	document.getElementById('fhair').style.bottom = '-32%';
+	document.getElementById('feyes').style.bottom = '-32%';
+	document.getElementById('febrow').style.bottom = '-32%';
+	document.getElementById('flips').style.bottom = '2%';
+	document.getElementById('fbeard').style.bottom = '-32%'; 
+}
+function beard () {
+	document.getElementById('fface').style.bottom = '-32%';
+	document.getElementById('fhair').style.bottom = '-32%';
+	document.getElementById('feyes').style.bottom = '-32%';
+	document.getElementById('febrow').style.bottom = '-32%';
+	document.getElementById('flips').style.bottom = '-32%';
+	document.getElementById('fbeard').style.bottom = '2%'; 
+}
+function face1 () {
+	var element = document.getElementById('face3_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('face2_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('face1_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+}
+function face2 () {
+	var element = document.getElementById('face3_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('face2_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+	element = document.getElementById('face1_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+}
+function face3 () {
+	var element = document.getElementById('face3_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+	element = document.getElementById('face2_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('face1_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+}
+function hair1 () {
+	var element = document.getElementById('hair3_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('hair2_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('hair1_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+}
+function hair2 () {
+	var element = document.getElementById('hair3_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('hair2_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+	element = document.getElementById('hair1_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+}
+function hair3 () {
+	var element = document.getElementById('hair3_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+	element = document.getElementById('hair2_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('hair1_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+}
+function eyes1 () {
+	var element = document.getElementById('eyes3_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('eyes2_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('eyes1_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+}
+function eyes2 () {
+	var element = document.getElementById('eyes3_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('eyes2_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+	element = document.getElementById('eyes1_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+}
+function eyes3 () {
+	var element = document.getElementById('eyes3_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+	element = document.getElementById('eyes2_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('eyes1_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+}
+function ebrow1 () {
+	var element = document.getElementById('ebrow3_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('ebrow2_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('ebrow1_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+}
+function ebrow2 () {
+	var element = document.getElementById('ebrow3_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('ebrow2_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+	element = document.getElementById('ebrow1_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+}
+function ebrow3 () {
+	var element = document.getElementById('ebrow3_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+	element = document.getElementById('ebrow2_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('ebrow1_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+}
+function lips3 () {
+	var element = document.getElementById('lips3_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+	element = document.getElementById('lips2_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('lips1_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+}
+function lips2 () {
+	var element = document.getElementById('lips3_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('lips2_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+	element = document.getElementById('lips1_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+}
+function lips1 () {
+	var element = document.getElementById('lips3_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('lips2_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('lips1_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+}
+function beard1 () {
+	var element = document.getElementById('beard3_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('beard2_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('beard1_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+}
+function beard2 () {
+	var element = document.getElementById('beard3_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('beard2_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+	element = document.getElementById('beard1_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+}
+function beard3 () {
+	var element = document.getElementById('beard3_xA0_Image_1_');
+	element.classList.remove("hide");
+	element.classList.add("show");
+	element = document.getElementById('beard2_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+	element = document.getElementById('beard1_xA0_Image_1_');
+	element.classList.remove("show");
+	element.classList.add("hide");
+}
